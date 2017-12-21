0bdf71aa1e5824289b3433bddebb8726013d0e3f
Pierre Thierry <pierre@nothos.net>
2017-12-21 15:56:35 +0100

    Import first version

---
+++ database.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# config
+
+PASSWORD=PASSWORD;
+USERNAME=USERNAME;
+DATABASE=DATABASE;
+
+CONTAINER=CONTAINER;
+
+
+
+if [[ -n "$1" ]]; then
+    ACTION=$1;
+else
+    echo "Usage: ./database.sh backup|restore [version]";
+    exit 1;
+fi
+
+if [[ -n "$2" ]]; then
+    VERSION="-$2";
+else
+    VERSION="";
+fi;
+
+
+FILE=./backup/backup${VERSION}.sql;
+AUTH="--user=${USERNAME} --password=${PASSWORD} ";
+
+PID=$(docker inspect --format '{{.State.Pid}}' $CONTAINER)
+
+
+run_in_container() {
+    nsenter -t $PID -m -u -i -p /bin/bash -c "$1";
+}
+
+
+case $ACTION in
+    backup)
+		COMMAND="mysqldump ${AUTH} ${DATABASE}";
+		run_in_container "$COMMAND" > "$FILE";;
+    restore)
+		COMMAND="mysql ${AUTH} ${DATABASE}";
+		run_in_container "$COMMAND" < $FILE;;
+esac;
